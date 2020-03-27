333b690272009d01e9c9890ccfebf89cdc1d0f70
Hoang Trinh <hoang.trinhj@gmail.com>
2020-03-27 10:55:51 +0700

    Update Help Page

--- src/client/config/constants.js
+++ src/client/config/constants.js
@@ -13,3 +13,10 @@ export const SUPPORTED_QUESTION_TYPES = {
   LIST: 'LIST',
   CHECKBOX: 'CHECKBOX',
 }
+
+export const URLS = {
+  TAWKTO_URL: 'https://tawk.to/chat/5e7c848c69e9320caabd351a/default',
+  FEEDBACK_FORM_URL:
+    'https://docs.google.com/forms/d/e/1FAIpQLSeU5FrtO-uC8FDf-08hQo3bEezIx616j6PYTcqxXYd8DkLMAw/viewform',
+  FAQ_URL: 'https://yace.jetbeaver.com/faq',
+}
--- src/client/containers/Help/index.jsx
+++ src/client/containers/Help/index.jsx
@@ -1,7 +1,85 @@
 import React from 'react'
+import { withStyles, makeStyles } from '@material-ui/core/styles'
+import { Button } from '@material-ui/core'
+import { Feedback, LiveHelp } from '@material-ui/icons'
+import { deepPurple, grey } from '@material-ui/core/colors'
+import styled from 'styled-components'
+
+import { URLS } from 'config/constants'
+
+const ColorButton = withStyles(theme => ({
+  root: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+    '&:hover': {
+      backgroundColor: deepPurple[300],
+    },
+  },
+}))(Button)
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  linkWrapper: {
+    width: '100%',
+    textAlign: 'center',
+    marginBottom: '1.5rem',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+}))
+
+export const VersionWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 100%;
+  bottom: 1rem;
+`
+
+export const Version = styled.span`
+  color: ${grey[400]};
+  font-weight: 500;
+`
 
 const Help = () => {
-  return <div>Help Page</div>
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.linkWrapper}>
+        <a
+          className={classes.link}
+          href={URLS.FEEDBACK_FORM_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ColorButton startIcon={<Feedback />} variant="contained">
+            Send A Feedback
+          </ColorButton>
+        </a>
+      </div>
+
+      <div className={classes.linkWrapper}>
+        <a className={classes.link} href={URLS.FAQ_URL} target="_blank" rel="noopener noreferrer">
+          <ColorButton startIcon={<LiveHelp />} variant="contained">
+            Check our FAQ
+          </ColorButton>
+        </a>
+      </div>
+
+      <VersionWrapper>
+        <Version>Version {process.env.ADDON_VERSION}</Version>
+      </VersionWrapper>
+    </div>
+  )
 }
 
 export default Help
