2732d896b5b75edfee5eb909eeb71a1384c05042
Hoang Trinh <hoang.trinhj@gmail.com>
2020-03-28 15:45:07 +0700

    Ensure Form Submit Trigger Exists

--- src/server/helpers/trigger.js
+++ src/server/helpers/trigger.js
@@ -6,9 +6,6 @@ export const adjustFormSubmitTrigger = () => {
   const form = FormApp.getActiveForm()
   const triggers = ScriptApp.getUserTriggers(form)
 
-  const documentProperties = PropertiesService.getDocumentProperties()
-  const triggerNeeded = documentProperties.getProperty('configuration') !== null
-
   // Create a new trigger if required; delete existing trigger
   //   if it is not needed.
   let existingTrigger = null
@@ -20,12 +17,12 @@ export const adjustFormSubmitTrigger = () => {
     }
   }
 
-  if (triggerNeeded && !existingTrigger) {
-    ScriptApp.newTrigger('respondToFormSubmit')
-      .forForm(form)
-      .onFormSubmit()
-      .create()
-  } else if (!triggerNeeded && existingTrigger) {
+  if (existingTrigger) {
     ScriptApp.deleteTrigger(existingTrigger)
   }
+
+  ScriptApp.newTrigger('respondToFormSubmit')
+    .forForm(form)
+    .onFormSubmit()
+    .create()
 }
