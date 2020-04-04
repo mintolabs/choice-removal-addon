b69fb0fbd3814e620b346a26d592a2ad6a1e1dbe
Hoang Trinh <hoang.trinhj@gmail.com>
2020-04-04 10:49:54 +0700

    Change Logo

--- src/client/components/Logo/index.jsx
+++ src/client/components/Logo/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import LogoSvg from 'icons/YACE_Logo.svg'
+import LogoSvg from 'icons/Choice_Removal_Logo.svg'
 
 const Logo = () => {
   return <LogoSvg width={40} height={40} />
