d395e00eb0587f648989d2ca0a827709f7b48265
Hoang Trinh <hoang.trinhj@gmail.com>
2020-04-25 10:24:16 +0700

    Update NoSupportedQuestions For Multiple Accouns

--- src/client/components/NoSupportedQuestion/index.jsx
+++ src/client/components/NoSupportedQuestion/index.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import { grey } from '@material-ui/core/colors'
+import { withStyles, makeStyles } from '@material-ui/core/styles'
+import { Button } from '@material-ui/core'
+import { LiveHelp } from '@material-ui/icons'
+import { deepPurple, grey, red } from '@material-ui/core/colors'
+
+import { URLS } from 'config/constants'
+
+const ColorButton = withStyles(theme => ({
+  root: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+    '&:hover': {
+      backgroundColor: deepPurple[300],
+    },
+  },
+}))(Button)
 
 const useStyles = makeStyles({
   root: {
@@ -11,6 +25,24 @@ const useStyles = makeStyles({
   title: {
     fontWeight: '500',
     marginBottom: '1rem',
+  },
+  linkWrapper: {
+    width: '100%',
+    textAlign: 'center',
+    marginBottom: '1.5rem',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+  emphasize: {
+    fontWeight: '500',
+    color: red[500],
+  },
+  multipleAccounts: {
+    borderTop: `1px solid ${grey[500]}`,
+    marginTop: '0.5rem',
+    marginBottom: '1rem',
+    paddingTop: '0.5rem',
   },
 })
 
@@ -28,7 +60,23 @@ const NoSupportedQuestion = () => {
         <li>Checkboxes</li>
       </ul>
 
-      <div>Please create at least one</div>
+      <div>Please create at least one.</div>
+
+      <div className={classes.multipleAccounts}>
+        Or maybe,{' '}
+        <span className={classes.emphasize}>
+          you are logging in with multiple Google accounts.{' '}
+        </span>
+        Click the button below!
+      </div>
+
+      <div className={classes.linkWrapper}>
+        <a className={classes.link} href={URLS.FAQ_URL} target="_blank" rel="noopener noreferrer">
+          <ColorButton startIcon={<LiveHelp />} variant="contained">
+            Check our FAQ
+          </ColorButton>
+        </a>
+      </div>
     </div>
   )
 }
